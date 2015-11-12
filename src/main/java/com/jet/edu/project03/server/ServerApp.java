cc5f3981e2db784e1fde66a8e18d9448fb437426
Sergey Trudov <trudov.sergey@gmail.com>
2015-11-12 13:22:32 +0300

    server

--- src/main/java/com/jet/edu/project03/server/ServerApp.java
+++ src/main/java/com/jet/edu/project03/server/ServerApp.java
@@ -1,8 +1,24 @@
 package com.jet.edu.project03.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ServerApp {
 
-    public static void main(String[] args) {
+    private ServerSocket serverSocket;
+    private List<Socket> socketList;
+    private Map<String, Socket> nameToSocketMap = new HashMap<>();
 
+    public ServerApp(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        start();
+    }
+
+    private void start() {
+        new Thread(new Acceptor(serverSocket, socketList, nameToSocketMap));
     }
 }
