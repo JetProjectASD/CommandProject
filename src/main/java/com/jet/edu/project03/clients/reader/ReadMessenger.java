273cca3ac7f9489e4f7b649bdf01e3f737323797
Dmitriy <reven911@gmail.com>
2015-11-13 16:50:39 +0300

    Refactoring ServerApp

--- src/main/java/com/jet/edu/project03/clients/reader/ReadMessenger.java
+++ src/main/java/com/jet/edu/project03/clients/reader/ReadMessenger.java
@@ -14,15 +14,17 @@ import static com.jet.edu.project03.clients.UtilitiesMessaging.*;
 public class ReadMessenger extends Thread {
     private String host;
     private int port;
+    private int localPort;
     private long id;
 
     /**
      * Constructor for inlalling ip addres and port
      * @param host IP address
      */
-    public ReadMessenger(String host, int port) {
+    public ReadMessenger(String host, int port, int localPort) {
         this.host = host;
         this.port = port;
+        this.localPort = localPort;
     }
 
     /**
@@ -30,32 +32,43 @@ public class ReadMessenger extends Thread {
      */
     @Override
     public void run() {
-        try(ServerSocket readerServerSocket = new ServerSocket(45000);
-            Socket socket = readerServerSocket.accept()) {
-            id = Long.parseLong(takeMessage(new BufferedReader(new InputStreamReader(socket.getInputStream()))));
-            System.out.println("id get");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        startLocalServerForSynchronizationLocalClients();
+        startReadingFromServer();
+    }
+
+    private void startReadingFromServer() {
         try (Socket socket = new Socket(host, port);
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
             sendMessage(writer, "READER USER_ID");
             sendMessage(writer, String.valueOf(id));
-            System.out.println("id send to server");
-            System.out.println(id);
+            System.out.println("id send to server: " + id);
 
             if ((takeMessage(reader).equals("OK"))) {
-                while (true) {
-                    try {
-                        ConsoleHelper.writeMessage(takeMessage(reader));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        break;
-                    }
-                }
+                writeToLocalConsole(reader);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToLocalConsole(BufferedReader reader) {
+        while (true) {
+            try {
+                ConsoleHelper.writeMessage(takeMessage(reader));
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    private void startLocalServerForSynchronizationLocalClients() {
+        try(ServerSocket readerServerSocket = new ServerSocket(localPort);
+            Socket socket = readerServerSocket.accept()) {
+            id = Long.parseLong(takeMessage(new BufferedReader(new InputStreamReader(socket.getInputStream()))));
+            System.out.println("id get");
         } catch (IOException e) {
             e.printStackTrace();
         }
