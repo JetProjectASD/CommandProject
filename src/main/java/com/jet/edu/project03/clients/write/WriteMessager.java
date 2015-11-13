6d68bbe0f35d275d4d525bd2a7f45e8e930d3323
Dmitriy <reven911@gmail.com>
2015-11-13 16:42:53 +0300

    Refactoring WriteMessager

--- src/main/java/com/jet/edu/project03/clients/write/WriteMessager.java
+++ src/main/java/com/jet/edu/project03/clients/write/WriteMessager.java
@@ -6,23 +6,26 @@ import com.jet.edu.project03.clients.write.exceptions.SomeException;
 import java.io.*;
 import java.net.Socket;
 
+import static com.jet.edu.project03.clients.UtilitiesMessaging.sendMessage;
+import static com.jet.edu.project03.clients.UtilitiesMessaging.takeMessage;
+
 /**
  * New thread for wtite user messages in console
  */
-import static com.jet.edu.project03.clients.UtilitiesMessaging.sendMessage;
-import static com.jet.edu.project03.clients.UtilitiesMessaging.takeMessage;
 
 public class WriteMessager extends Thread {
 
     private final String host;
     private final int port;
+    private int localPort;
 
     /**
      * Constructor which install IP address and port
      */
-    public WriteMessager(String host, int port) {
+    public WriteMessager(String host, int port, int localPort) {
         this.host = host;
         this.port = port;
+        this.localPort = localPort;
     }
 
     /**
@@ -35,39 +38,48 @@ public class WriteMessager extends Thread {
              BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
             sendMessage(writer, "CONNECT");
-            long sessionId = Long.parseLong(takeMessage(reader)); // Тут нужно как-то обработать, а вдруг придет не лонг оО
-            System.out.println("id get");
-            System.out.println(sessionId);
-            if ((takeMessage(reader).equals("OK"))) {
-                try (Socket socket = new Socket("127.0.0.1", 45000)) {
-                    BufferedWriter writeToReader = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    sendMessage(writeToReader, String.valueOf(sessionId));
-                }
-                String str;
-                while ((str = ConsoleHelper.readString()) != null) {
-                    String[] split = str.split(" ");
-                    try {
-                        String message;
-                        if (!split[0].isEmpty() && (message = deletePrefixFromMessage(split[0], str)).length() < 150) {
-                            String commandToServer = IDsFiltering.getPrefix(split[0]);
-                            System.out.println(commandToServer);
-                            sendMessage(writer, commandToServer);
-                            sendMessage(writer, message);
-                            System.out.println(commandToServer + " " + message);
-                        }
-                    } catch (SomeException e) {
-                        ConsoleHelper.writeMessage("Ввод не корректен.");
-                        ConsoleHelper.writeMessage(e.getMessage());
-                    }
-                }
-            }
+            long sessionId = Long.parseLong(takeMessage(reader));
+            System.out.println("id get: " + sessionId);
 
+            if ((takeMessage(reader).equals("OK"))) {
+                sendIdToReader(sessionId);
+                readConsole(writer);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void readConsole(BufferedWriter writer) throws IOException {
+        String str;
+        while ((str = ConsoleHelper.readString()) != null) {
+            String[] split = str.split(" ");
+            try {
+                String message;
+                if (!split[0].isEmpty() && (message = deletePrefixFromMessage(split[0], str)).length() < 150) {
+                    sendCommandAndMessage(writer, split[0], message);
+                }
+            } catch (SomeException e) {
+                ConsoleHelper.writeMessage("Ввод не корректен.");
+            }
+        }
+    }
+
+    private void sendCommandAndMessage(BufferedWriter writer, String prefix, String message) throws SomeException, IOException {
+        String commandToServer = IDsFiltering.getPrefix(prefix);
+        sendMessage(writer, commandToServer);
+        sendMessage(writer, message);
+        ConsoleHelper.writeMessage(commandToServer + " " + message);
+    }
+
+    private void sendIdToReader(long sessionId) throws IOException {
+        try (Socket socket = new Socket(host, localPort)) {
+            BufferedWriter writeToReader = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            sendMessage(writeToReader, String.valueOf(sessionId));
+        }
+    }
+
     private String deletePrefixFromMessage(String prefix, String message) {
-        return message.replaceFirst(prefix, "");
+        return message.replaceFirst(prefix + " ", "");
     }
 }
