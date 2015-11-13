910810be6f4ea8092ef72be9c1bd5a049ae15835
Dmitriy <reven911@gmail.com>
2015-11-13 16:59:05 +0300

    ReaderMessage refactoring

--- src/main/java/com/jet/edu/project03/clients/reader/ReadMessenger.java
+++ src/main/java/com/jet/edu/project03/clients/reader/ReadMessenger.java
@@ -6,10 +6,12 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import static com.jet.edu.project03.clients.UtilitiesMessaging.sendMessage;
+import static com.jet.edu.project03.clients.UtilitiesMessaging.takeMessage;
+
 /**
  * New thrad for reading messages from server
  */
-import static com.jet.edu.project03.clients.UtilitiesMessaging.*;
 
 public class ReadMessenger extends Thread {
     private final int localPort;
@@ -32,6 +34,11 @@ public class ReadMessenger extends Thread {
      */
     @Override
     public void run() {
+        startLocalServerForSynchronizationLocalClients();
+        startReadingMessageFromServer();
+    }
+
+    private void startReadingMessageFromServer() {
         try (Socket socket = new Socket(host, port);
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
