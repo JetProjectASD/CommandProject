4c369a51e320732525b634e3c55044ea0e04e84d
Dmitriy <reven911@gmail.com>
2015-11-14 11:12:20 +0300

    Add output history and add chat in room

--- src/main/java/com/jet/edu/project03/server/ServerApp.java
+++ src/main/java/com/jet/edu/project03/server/ServerApp.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Server
@@ -14,26 +17,28 @@ public class ServerApp {
 
     private final ServerSocket serverSocket;
     private final List<User> users = new LinkedList<>();
-    private final ExecutorService pool = Executors.newFixedThreadPool(50);
-
-
-    /**
-     * Constructor which in start server
-     */
-    public ServerApp(int port) throws IOException {
-        this.serverSocket = new ServerSocket(port);
-    }
+    public final static ExecutorService pool = Executors.newFixedThreadPool(50);
+    public final static List<String> lastTenChatMessages = new LinkedList<>();
+    Logger logger;
 
     /**
      * Start port and install port and IP address
      */
     public static void main(String[] args) {
+        ServerApp server = new ServerApp(Integer.parseInt(args[0]));
+        server.start();
+    }
+
+    /**
+     * Constructor which install server
+     */
+    public ServerApp(int port) {
         try {
-            ServerApp server = new ServerApp(40001);
-            System.out.println("Server start...");
-            server.start();
+            loggerInit();
+            this.serverSocket = new ServerSocket(port);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING, "Unable to open server socket with " + port + " port", e);
+            throw new RuntimeException("blabla",e);
         }
     }
 
@@ -41,7 +46,42 @@ public class ServerApp {
      * Start thread which listen port
      */
     public void start() {
-        pool.submit(new Acceptor(pool, serverSocket, users));
+        logger.log(Level.INFO, "Server start successfully");
+        pool.submit(new Acceptor(serverSocket, users));
+//        new Thread(() -> {
+//            String string = ConsoleHelper.readString();
+//            if(string.equals("stop")) {
+//                try {
+//                    stop();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
     }
 
+    /*private void stop() throws IOException {
+        pool.shutdown();
+        System.out.println("pool shutdown");
+//        synchronized (users) {
+//            for(User user : users) {
+//                if(!user.getUserSocket().isClosed()) {
+//                    user.getUserSocket().close();
+//                }
+//            }
+//        }
+
+        serverSocket.close();
+
+    }*/
+
+    private void loggerInit() {
+        logger = Logger.getLogger(ServerApp.class.getName());
+        try {
+            FileHandler fileHandler = new FileHandler("ServerLog.log");
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Log error because I/O breaking", e);
+        }
+    }
 }
